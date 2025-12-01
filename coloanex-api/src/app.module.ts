6d38605ac9391edc14b0dcfe01086966277ef251
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-01 22:03:54 +0545

    Updated application module to integrate new user management components

--- coloanex-api/src/app.module.ts
+++ coloanex-api/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RolesModule } from './roles/roles.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [RolesModule],
+  imports: [RolesModule, PermissionsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
