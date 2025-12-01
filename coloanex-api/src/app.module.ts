939148d111dc8bf039dd4a4530f4d2aa96112853
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-01 21:20:22 +0545

    Updated application module to integrate new role management components

--- coloanex-api/src/app.module.ts
+++ coloanex-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [],
+  imports: [RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
