d081cba561f08a8aff306af814152434d679cc4e
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-01 21:20:22 +0545

    Added DTO to support structured role update operations

---
+++ coloanex-api/src/roles/dto/update-role.dto.ts
@@ -0,0 +1,8 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateRoleDto } from './create-role.dto';
+import { IsOptional } from 'class-validator';
+
+export class UpdateRoleDto extends PartialType(CreateRoleDto) {
+  @IsOptional()
+  id?: bigint;
+}
