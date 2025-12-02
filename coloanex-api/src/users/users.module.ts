ba865c571a475d98a3e9a6e3308aef37c1035064
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-02 11:59:19 +0545

    Reorganized users module to improve service and provider alignment.

--- coloanex-api/src/users/users.module.ts
+++ coloanex-api/src/users/users.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma.service';
+import { ActivityLogsModule } from '../activity-logs/activity-logs.module';
+import { BorrowersModule } from '../borrowers/borrowers.module';
 
 @Module({
+  imports: [ActivityLogsModule, BorrowersModule],
   controllers: [UsersController],
   providers: [UsersService, PrismaService],
+  exports: [UsersService],
 })
 export class UsersModule {}
