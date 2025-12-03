888c34e4faf7b89a4053c74ec523a2714ca231e3
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-03 19:00:43 +0545

    Reconfigured permissions module for better dependency management

--- coloanex-api/src/permissions/permissions.module.ts
+++ coloanex-api/src/permissions/permissions.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { PermissionsController } from './permissions.controller';
 import { PrismaService } from 'src/prisma.service';
+import { ActivityLogsModule } from '../activity-logs/activity-logs.module';
 
 @Module({
+  imports: [ActivityLogsModule],
   controllers: [PermissionsController],
   providers: [PermissionsService, PrismaService],
+  exports: [PermissionsService],
 })
 export class PermissionsModule {}
