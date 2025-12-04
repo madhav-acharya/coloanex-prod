5b29f8fcc00df05d74e63f626c80d1785293fb94
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-04 23:41:34 +0545

    Updated main application entry point

--- coloanex-api/src/main.ts
+++ coloanex-api/src/main.ts
@@ -4,6 +4,9 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -13,6 +16,20 @@ async function bootstrap() {
       },
     }),
   );
+
+  app.enableCors({
+    origin: [process.env.CORS_ORIGIN_URL],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+      'Accept',
+      'Origin',
+    ],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
