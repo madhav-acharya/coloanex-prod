4b2b34b31ffcdeff02d2596a180b4c130789692a
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-04 23:41:34 +0545

    Updated activity logs service

--- coloanex-api/src/activity-logs/activity-logs.service.ts
+++ coloanex-api/src/activity-logs/activity-logs.service.ts
@@ -5,7 +5,7 @@ import {
   ActivityEntityType,
   ActivityAction,
 } from './entities/activity-log.entity';
-import type { Prisma } from '@prisma/client';
+import type { Prisma, $Enums } from '@prisma/client';
 
 @Injectable()
 export class ActivityLogsService {
@@ -29,7 +29,7 @@ export class ActivityLogsService {
         tenantId: data.tenantId,
         entityType: data.entityType,
         entityId: data.entityId,
-        action: data.action as any,
+        action: data.action as $Enums.ActivityAction,
         description: data.description,
         before: data.before
           ? (data.before as Prisma.InputJsonValue)
