19225801a15f82d87ba62dca4df6eccd67c26599
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-08 13:53:30 +0545

    Updated update role DTO

--- coloanex-api/src/roles/dto/update-role.dto.ts
+++ coloanex-api/src/roles/dto/update-role.dto.ts
@@ -1,8 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRoleDto } from './create-role.dto';
-import { IsOptional } from 'class-validator';
+import { IsOptional, IsString, IsNotEmpty } from 'class-validator';
+import { CapitalizeFirstLetter } from '../../common/decorators/capitalize-first-letter.decorator';
 
 export class UpdateRoleDto extends PartialType(CreateRoleDto) {
   @IsOptional()
   id?: bigint;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @CapitalizeFirstLetter()
+  name?: string;
 }
