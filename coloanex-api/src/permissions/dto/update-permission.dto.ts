8a103c02f30678da39eb322f53d9a7cc18edf999
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-08 13:53:30 +0545

    Updated update permission DTO

--- coloanex-api/src/permissions/dto/update-permission.dto.ts
+++ coloanex-api/src/permissions/dto/update-permission.dto.ts
@@ -1,8 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePermissionDto } from './create-permission.dto';
-import { IsOptional } from 'class-validator';
+import { IsOptional, IsString, IsNotEmpty } from 'class-validator';
+import { CapitalizeFirstLetter } from '../../common/decorators/capitalize-first-letter.decorator';
 
 export class UpdatePermissionDto extends PartialType(CreatePermissionDto) {
   @IsOptional()
   id?: bigint;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @CapitalizeFirstLetter()
+  name?: string;
 }
