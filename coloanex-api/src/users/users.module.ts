f12e520a32942b93f5e869a7f59293d8179becd6
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-21 18:20:05 +0545

    Modified users module imports

--- coloanex-api/src/users/users.module.ts
+++ coloanex-api/src/users/users.module.ts
@@ -3,10 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma.service';
 import { ActivityLogsModule } from '../activity-logs/activity-logs.module';
-import { BorrowersModule } from '../borrowers/borrowers.module';
+import { PermissionsModule } from '../permissions/permissions.module';
 
 @Module({
-  imports: [ActivityLogsModule, BorrowersModule],
+  imports: [ActivityLogsModule, PermissionsModule],
   controllers: [UsersController],
   providers: [UsersService, PrismaService],
   exports: [UsersService],
