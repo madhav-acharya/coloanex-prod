ca9b32d2ca564c908c27a8f4f830d6d0e90222b2
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-21 18:20:05 +0545

    Adjusted permissions module configuration

--- coloanex-api/src/permissions/permissions.module.ts
+++ coloanex-api/src/permissions/permissions.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { PermissionsController } from './permissions.controller';
+import { PermissionAssignmentService } from './permission-assignment.service';
 import { PrismaService } from 'src/prisma.service';
 import { ActivityLogsModule } from '../activity-logs/activity-logs.module';
 
 @Module({
   imports: [ActivityLogsModule],
   controllers: [PermissionsController],
-  providers: [PermissionsService, PrismaService],
-  exports: [PermissionsService],
+  providers: [PermissionsService, PermissionAssignmentService, PrismaService],
+  exports: [PermissionsService, PermissionAssignmentService],
 })
 export class PermissionsModule {}
