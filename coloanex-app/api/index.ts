c8792e14d92870aca4e17b194823b3a732d71179
Madhav Acharya <np05cp4s240068@iic.edu.np>
2025-12-30 23:43:22 +0545

    Created an index file to export all API modules for easier imports and maintainability.

---
+++ coloanex-app/api/index.ts
@@ -0,0 +1,6 @@
+export { authApi } from "./authApi";
+export { lendersApi } from "./lendersApi";
+export { loansApi } from "./loansApi";
+export { kycApi } from "./kycApi";
+export { usersApi } from "./usersApi";
+export { default as apiClient } from "./client";
