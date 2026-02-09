4175bc74ef68613e7656ac55cfd3de3db55a3bac
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-02-09 12:35:00 +0545

    Added the MailModule import to the Loans module to support loan status email notifications.

--- coloanex-api/src/loans/loans.module.ts
+++ coloanex-api/src/loans/loans.module.ts
@@ -3,9 +3,10 @@ import { LoansService } from './loans.service';
 import { LoansController } from './loans.controller';
 import { PrismaService } from '../prisma.service';
 import { ActivityLogsModule } from '../activity-logs/activity-logs.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [ActivityLogsModule],
+  imports: [ActivityLogsModule, MailModule],
   controllers: [LoansController],
   providers: [LoansService, PrismaService],
   exports: [LoansService],
