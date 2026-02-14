890e0175130cf575a472ddf847fbcef50e4d7680
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-02-14 23:39:12 +0545

    Reorganized the store index to support new slices and improve state management structure.

--- coloanex-web/src/store/index.ts
+++ coloanex-web/src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import themeReducer from "./slices/themeSlice";
 import { baseApi } from "@/apis/baseApi";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    theme: themeReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
