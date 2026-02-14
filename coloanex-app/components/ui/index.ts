227cf11281625e8e0cef259851cb99d977e6af76
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-02-14 23:39:11 +0545

    Updated the UI components index to export new and improved components for easier imports.

--- coloanex-app/components/ui/index.ts
+++ coloanex-app/components/ui/index.ts
@@ -5,4 +5,5 @@ export { default as LenderLogo } from "./LenderLogo";
 export { default as RatingStars } from "./RatingStars";
 export { default as DatePickerInput } from "./DatePickerInput";
 export { default as PickerInput } from "./PickerInput";
+export { default as AppHeader } from "./AppHeader";
 export { ToastProvider, useToast } from "./Toast";
