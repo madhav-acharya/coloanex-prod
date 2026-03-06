11b50fecce388ae6752bda6e79f1d0b6249f75f1
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-03-06 23:49:03 +0545

    Exported updated APIs

--- coloanex-app/api/index.ts
+++ coloanex-app/api/index.ts
@@ -7,7 +7,15 @@ export { notificationsApi } from "./notificationsApi";
 export { rulesApi } from "./rulesApi";
 export { contractsApi } from "./contractsApi";
 export { paymentSchedulesApi } from "./paymentSchedulesApi";
+export { paymentsApi } from "./paymentsApi";
 export { transactionsApi } from "./transactionsApi";
 export { walletsApi } from "./walletsApi";
 export { default as analyticsApi } from "./analyticsApi";
 export { default as apiClient } from "./client";
+export type {
+  PaymentGateway,
+  TransactionType,
+  InitiatePaymentPayload,
+  InitiatePaymentResult,
+  VerifyPaymentResult,
+} from "./paymentsApi";
