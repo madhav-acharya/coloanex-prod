2fb9a30e637439eca607e55d58e2a8ee7ef6192c
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-04-05 23:17:28 +0545

    Removed coloanex-api/src/solana/solana.module.ts.

--- coloanex-api/src/solana/solana.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SolanaService } from './solana.service';
-
-@Module({
-  providers: [SolanaService],
-  exports: [SolanaService],
-})
-export class SolanaModule {}
