9798856791070e2c9071e1dff49fba76c999a3df
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-04-06 23:53:52 +0545

    Modified store/index.ts to register new slices and improve state management for recent features.

--- coloanex-web/src/store/index.ts
+++ coloanex-web/src/store/index.ts
@@ -2,17 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import themeReducer from "./slices/themeSlice";
 import { baseApi } from "@/apis/baseApi";
-import { configApi } from "@/apis/configApi";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     theme: themeReducer,
     [baseApi.reducerPath]: baseApi.reducer,
-    [configApi.reducerPath]: configApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware, configApi.middleware),
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
