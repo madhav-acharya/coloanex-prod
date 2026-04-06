1ec9482c50696eff0391e0be8064f94889677bec
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-04-06 23:53:52 +0545

    Updated loan-details.tsx to display blockchain-related loan information and improve data presentation for users.

--- coloanex-app/app/loans/loan-details.tsx
+++ coloanex-app/app/loans/loan-details.tsx
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   ActivityIndicator,
   Image,
+  Linking,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useLocalSearchParams, router } from "expo-router";
