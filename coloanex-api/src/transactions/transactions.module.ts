50e93c23ee749b8a0fb19edbacd1cd6d0063548e
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-04-08 23:10:01 +0545

    Updated the transactions module to register new providers or dependencies required for transaction features.

--- coloanex-api/src/transactions/transactions.module.ts
+++ coloanex-api/src/transactions/transactions.module.ts
@@ -2,11 +2,11 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { PrismaService } from '../prisma.service';
-import { WalletsModule } from '../wallets/wallets.module';
+
 import { BlockchainModule } from '../blockchain/blockchain.module';
 
 @Module({
-  imports: [forwardRef(() => WalletsModule), BlockchainModule],
+  imports: [BlockchainModule],
   controllers: [TransactionsController],
   providers: [TransactionsService, PrismaService],
   exports: [TransactionsService],
