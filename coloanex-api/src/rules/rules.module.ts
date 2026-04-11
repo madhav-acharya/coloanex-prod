696cfbce0c1383b50bb3285dcf25e118619aa829
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-04-11 23:52:39 +0545

    Updated rules module registration to support orchestrator-driven rule evaluation and enforcement.

--- coloanex-api/src/rules/rules.module.ts
+++ coloanex-api/src/rules/rules.module.ts
@@ -3,9 +3,10 @@ import { RulesService } from './rules.service';
 import { RulesController } from './rules.controller';
 import { PrismaService } from '../prisma.service';
 import { BlockchainModule } from '../blockchain/blockchain.module';
+import { TransactionOrchestratorModule } from '../transaction-orchestrator/transaction-orchestrator.module';
 
 @Module({
-  imports: [BlockchainModule],
+  imports: [BlockchainModule, TransactionOrchestratorModule],
   controllers: [RulesController],
   providers: [RulesService, PrismaService],
   exports: [RulesService],
