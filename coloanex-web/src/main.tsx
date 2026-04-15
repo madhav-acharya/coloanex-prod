fe3cb1881af516ff64ff3a150c1163e6a5af23df
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-04-15 23:03:23 +0545

    Improved application initialization and error handling for a smoother startup.

--- coloanex-web/src/main.tsx
+++ coloanex-web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "next-themes";
 import { store } from "./store/index.ts";
 import { Toaster } from "./components/ui/sonner";
 import "./index.css";
@@ -10,10 +11,12 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-        <Toaster position="top-right" expand={false} richColors />
-      </BrowserRouter>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <BrowserRouter>
+          <App />
+          <Toaster position="top-right" expand={false} />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </StrictMode>,
 );
